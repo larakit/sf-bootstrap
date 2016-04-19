9f67815dafa903787a7079ca5e7a2fe9a55be539
Igor Babichev (himic) <himic@himic.ru>
2016-04-19 10:41:22 +0000

    Imported from SVN by Bitbucket

---
+++ init.php
@@ -0,0 +1,5 @@
+<?php
+\Larakit\StaticFiles\Manager::package('larakit/sf-bootstrap')
+                            ->usePackage('larakit/sf-jquery')
+                            ->js('//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js')
+                            ->css('//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.min.css');
