5e0af2aefe52beb50db269d48f6f4b84b35940d1
Alexey Berdnikov <aberdnikov@gmail.com>
2016-05-04 10:34:31 +0500

    DNS prefetch

--- init.php
+++ init.php
@@ -3,3 +3,4 @@
                             ->usePackage('larakit/sf-jquery')
                             ->js('//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js')
                             ->css('//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.min.css');
+\Larakit\Page\PageDnsPrefetch::register('//cdnjs.cloudflare.com');
